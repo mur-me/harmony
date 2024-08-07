ea75f8a13cad68a42f6d8c4522d4db14b2a06a50
Uladzislau Muraveika <uladzislau.muraveika@protofire.io>
2024-08-07 16:51:13 +0300

    feat(Dockerfile) - add argument TEST_REPO_BRANCH to point add possibility to point tests repo to non-master branch via CI, add MAIN_REPO_BRANCH to point exaclty the same repo version in the test run

--- scripts/travis_rpc_checker.sh
+++ scripts/travis_rpc_checker.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-echo $TRAVIS_PULL_REQUEST_BRANCH
+TEST_REPO_BRANCH=${TEST_REPO_BRANCH:-master}
+echo "Working on '${TEST_REPO_BRANCH}' branch"
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-echo $DIR
-echo $GOPATH
-cd $GOPATH/src/github.com/harmony-one/harmony-test
-git fetch
-git checkout $TRAVIS_PULL_REQUEST_BRANCH || true
-git pull
-git branch --show-current
+echo "Working dir is ${DIR}"
+echo "GOPATH is ${GOPATH}"
+cd "${GOPATH}/src/github.com/harmony-one/harmony-test"
+# cover possible force pushes to remote branches - just rebase local on top of origin
+git checkout "${TEST_REPO_BRANCH}"
+git pull --rebase=true
 cd localnet
-docker build -t harmonyone/localnet-test .
+docker build --build-arg MAIN_REPO_BRANCH="$(git rev-parse --abbrev-ref HEAD)" -t harmonyone/localnet-test .
 docker run -v "$DIR/../:/go/src/github.com/harmony-one/harmony" harmonyone/localnet-test -n
